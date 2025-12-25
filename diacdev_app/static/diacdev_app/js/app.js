bea7fe3d4a18fd6c2627b1118456d03ae657930f
DIACAID <tome.its02@gmail.com>
2025-12-25 08:57:12 -0300

    feat(index.html): agregando nueva paleta de colores y modo dark:light

---
+++ diacdev_app/static/diacdev_app/js/app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("theme-toggle");
+
+  if (!toggle) return;
+
+  toggle.addEventListener("click", () => {
+    const currentTheme =
+      document.documentElement.getAttribute("data-theme") || "light";
+
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+    document.documentElement.setAttribute("data-theme", newTheme);
+  });
+});
